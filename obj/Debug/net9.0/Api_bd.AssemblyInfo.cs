b6208f0be48b9b3a4536a1d5541febe2c36996fc
evellynfurtad0 <evellynfurtado100@gmail.com>
2025-12-08 12:59:36 -0300

    atualizando

--- obj/Debug/net9.0/Api_bd.AssemblyInfo.cs
+++ obj/Debug/net9.0/Api_bd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_bd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f15b2312b18260c40366844b319aa872a587d17f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00620da8d3b945b0af893b8162df720c003455d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_bd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_bd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
